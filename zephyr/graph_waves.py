0fd2cce65048caab4b29556e4fdf98419c2188db
Louis Goessling <louis@goessling.com>
2021-11-13 19:00:46 -0600

    Add some tools to graph the script

---
+++ zephyr/graph_waves.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import csv
+import sys
+
+# Can pass a comma-seperated list of fields to graph, e.g. python3 graph_waves.py led.r,led.g,led.b
+
+if len(sys.argv)==2:
+	toshow = sys.argv[1].split(',')
+else:
+	toshow = ['erm.power']
+
+with open('output.csv', 'r') as fd:
+	r = csv.reader(fd)
+	header, *rows = list(r)
+
+timestamps = [x[header.index("timestamp")] for x in rows]
+seriesdata = {}
+for item in header:
+	if item != "timestamp" and item in toshow:
+		seriesdata[item] = [x[header.index(item)] for x in rows]
+
+fig, ax = plt.subplots()
+for series, values in seriesdata.items():
+	ax.plot(timestamps, values, label=series)
+ax.legend()
+
+ax.set(xlabel='Time (s)', ylabel='Values')
+ax.grid()
+
+plt.show()
--- zephyr/run_waves.lua
+++ zephyr/run_waves.lua
@@ -1,5 +1,7 @@
 
---https://gist.github.com/justnom/9816256
+io = require('io')
+
+-- Adapted from https://gist.github.com/justnom/9816256
 function table_to_string(tbl)
     local result = "{"
     for k, v in pairs(tbl) do
@@ -51,13 +53,28 @@ setup = {
 
 iterator = waves(setup)
 
+file = io.open("output.csv", "w")
+
+file:write("timestamp,tens.pulse,tens.frequency,erm.power,led.r,led.g,led.b\n")
+
 adjust = 0xffff
 timestamp = 0
-while (timestamp < 10) do
+while (timestamp < 20) do
 	v = iterator(timestamp, adjust)
+
 	print('ts=' .. timestamp)
 	print('\t TENS = ' .. table_to_string(v[1]))
 	print('\t ERM  = ' .. table_to_string(v[2]))
 	print('\t LED  = ' .. table_to_string(v[3]))
+
+	file:write(table.concat({
+		timestamp,
+		v[1].pulse, v[1].frequency,
+		v[2].power,
+		v[3].values.r, v[3].values.g, v[3].values.b
+	}, ",").."\n")
+
 	timestamp = timestamp + 0.1
 end
+
+file:close()
