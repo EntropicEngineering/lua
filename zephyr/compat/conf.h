0f344b03878a5daca444f4de263b1df2e0b27ca7
Louis Goessling <louis@goessling.com>
2021-11-30 16:49:23 -0600

    Gate library compilation on kconfig options, fix bug

--- zephyr/compat/conf.h
+++ zephyr/compat/conf.h
@@ -1,4 +1,5 @@
 
 // Included by luaconf.h, bounces settings to KConfig options
 
-#define lua_getlocaledecpoint() CONFIG_LUA_LOCALE_DECPOINT
+// Silly workaround because Kconfig parameter is quoted
+#define lua_getlocaledecpoint() (CONFIG_LUA_LOCALE_DECPOINT[0])
--- zephyr/compat/prefix.h
+++ zephyr/compat/prefix.h
@@ -6,19 +6,19 @@
 
 #define l_signalT int
 
-#ifdef LUA_EXCEPTION_IMPLEMENTATION_PANIC
+#ifdef CONFIG_LUA_EXCEPTION_IMPLEMENTATION_PANIC
 #define LUAI_THROW(L,c) lua_compat_throw(L,c) //panics!
 #define LUAI_TRY(L,c,a) { a }
 #define luai_jmpbuf int //dummy, nothing stored
-#endif // LUA_EXCEPTION_IMPLEMENTATION_PANIC
+#endif // CONFIG_LUA_EXCEPTION_IMPLEMENTATION_PANIC
 
-#ifdef LUA_EXCEPTION_IMPLEMENTATION_GCC_LONGJMP
+#ifdef CONFIG_LUA_EXCEPTION_IMPLEMENTATION_GCC_LONGJMP
 // Implemented per https://gcc.gnu.org/onlinedocs/gcc/Nonlocal-Gotos.html
 typedef intptr_t         __jmp_buf[5];
 #define LUAI_THROW(L,c)  __builtin_longjmp((c)->b, 1) 
 #define LUAI_TRY(L,c,a)  if (__builtin_setjmp((c)->b) == 0) { a }
 #define luai_jmpbuf      __jmp_buf
-#endif // LUA_EXCEPTION_IMPLEMENTATION_GCC_LONGJMP
+#endif // CONFIG_LUA_EXCEPTION_IMPLEMENTATION_GCC_LONGJMP
 
 #define lua_writestring(s, l) lua_compat_writestring(s, l)
 #define lua_writestringerror(s, p) lua_compat_writestringerror(s, p)
